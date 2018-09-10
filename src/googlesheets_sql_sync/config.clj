6ab8dee4d8f4df9f39f5ee36c289eafc6d684382
Jorin Vogel <hi@jorin.me>
2018-09-10 08:44:14 +0200

    Move spec to separate file

--- src/googlesheets_sql_sync/config.clj
+++ src/googlesheets_sql_sync/config.clj
@@ -2,9 +2,8 @@
   (:require
    [cheshire.core :as json]
    [clojure.java.io :as io]
-   [clojure.java.jdbc.spec :as jdbc-spec]
-   [clojure.spec.alpha :as s]
-   [googlesheets-sql-sync.web :refer [oauth-route]]))
+   [googlesheets-sql-sync.web :refer [oauth-route]]
+   [googlesheets-sql-sync.spec :as spec]))
 
 (defn- default-config [{:keys [port]}]
   {:sheets [{:table          "your_sql_table_name"
@@ -19,81 +18,13 @@
                         :redirect_uri  (str "http://localhost:" port oauth-route)}
    :interval {:minutes 30}})
 
-(s/def ::str-not-empty (s/and string? not-empty))
-
-(s/def ::table ::str-not-empty)
-(s/def ::spreadsheet_id ::str-not-empty)
-(s/def ::target ::str-not-empty)
-(s/def ::sheets (s/coll-of (s/keys :req-un [::table]
-                                   ::spreadsheet_id
-                                   ::target)))
-
-(s/def ::targets (s/map-of keyword?
-                           ::jdbc-spec/db-spec))
-
-(defn- valid-url? [s]
-  (try
-    (.toURL (java.net.URI. s))
-    true
-    (catch Exception e false)))
-
-(comment
-  (true? (valid-url? "http://localhost:80"))
-  (false? (valid-url? "http//localhost:80")))
-
-(s/def ::client_id ::str-not-empty)
-(s/def ::client_secret ::str-not-empty)
-(s/def ::redirect_uri valid-url?)
-(s/def ::access_token ::str-not-empty)
-(s/def ::expires_in pos-int?)
-(s/def ::refresh_token ::str-not-empty)
-
-(s/def ::google_credentials (s/keys :req-un [::client_id
-                                             ::client_secret
-                                             ::redirect_uri]
-                                    :opt-un [::access_token
-                                             ::expires_in
-                                             ::refresh_token]))
-
-(s/def ::days pos-int?)
-(s/def ::hours pos-int?)
-(s/def ::minutes pos-int?)
-(s/def ::seconds pos-int?)
-(s/def ::interval (s/and (s/keys :req-un [(or ::days
-                                              ::hours
-                                              ::minutes
-                                              ::seconds)])))
-
-(comment
-  (s/valid? ::interval (:interval (default-config {:port 1234}))))
-
-(defn- targets-exist? [{:keys [sheets targets]}]
-  (every? #(targets (keyword (:target %))) sheets))
-
-(comment
-  (targets-exist? {:sheets [{:target "a"} {:target "b"}]
-                   :targets {:a 1 :b {}}})
-  (false? (targets-exist? {:sheets [{:target "a"}]
-                           :targets {:b 1}})))
-
-(s/def ::config (s/and (s/keys :req-un [::sheets
-                                        ::targets
-                                        ::google_credentials
-                                        ::interval])
-                       targets-exist?))
-
-(defn- valid-config [data]
-  (when-not (s/valid? ::config config)
-    (throw (Exception. (s/explain-str ::config config))))
-  config)
-
 (defn- write-file [data config-file]
-  (->> (json/generate-string (valid-config data) {:pretty true})
+  (->> (json/generate-string (spec/valid-config data) {:pretty true})
        (spit config-file))
   data)
 
 (defn read-file [config-file]
-  (valid-config (json/parse-string (slurp config-file) true)))
+  (spec/valid-config (json/parse-string (slurp config-file) true)))
 
 (defn generate
   ([ctx]
---
+++ src/googlesheets_sql_sync/spec.clj
@@ -0,0 +1,72 @@
+(ns googlesheets-sql-sync.spec
+  (:require
+   [clojure.java.jdbc.spec :as jdbc-spec]
+   [clojure.spec.alpha :as s]))
+
+(s/def ::str-not-empty (s/and string? not-empty))
+
+(s/def ::table ::str-not-empty)
+(s/def ::spreadsheet_id ::str-not-empty)
+(s/def ::target ::str-not-empty)
+(s/def ::sheets (s/coll-of (s/keys :req-un [::table]
+                                   ::spreadsheet_id
+                                   ::target)))
+
+(s/def ::targets (s/map-of keyword?
+                           ::jdbc-spec/db-spec))
+
+(defn- valid-url? [s]
+  (try
+    (.toURL (java.net.URI. s))
+    true
+    (catch Exception e false)))
+
+(comment
+  (true? (valid-url? "http://localhost:80"))
+  (false? (valid-url? "http//localhost:80")))
+
+(s/def ::client_id ::str-not-empty)
+(s/def ::client_secret ::str-not-empty)
+(s/def ::redirect_uri valid-url?)
+(s/def ::access_token ::str-not-empty)
+(s/def ::expires_in pos-int?)
+(s/def ::refresh_token ::str-not-empty)
+
+(s/def ::google_credentials (s/keys :req-un [::client_id
+                                             ::client_secret
+                                             ::redirect_uri]
+                                    :opt-un [::access_token
+                                             ::expires_in
+                                             ::refresh_token]))
+
+(s/def ::days pos-int?)
+(s/def ::hours pos-int?)
+(s/def ::minutes pos-int?)
+(s/def ::seconds pos-int?)
+(s/def ::interval (s/and (s/keys :req-un [(or ::days
+                                              ::hours
+                                              ::minutes
+                                              ::seconds)])))
+
+(comment
+  (s/valid? ::interval (:interval (default-config {:port 1234}))))
+
+(defn- targets-exist? [{:keys [sheets targets]}]
+  (every? #(targets (keyword (:target %))) sheets))
+
+(comment
+  (targets-exist? {:sheets [{:target "a"} {:target "b"}]
+                   :targets {:a 1 :b {}}})
+  (false? (targets-exist? {:sheets [{:target "a"}]
+                           :targets {:b 1}})))
+
+(s/def ::config (s/and (s/keys :req-un [::sheets
+                                        ::targets
+                                        ::google_credentials
+                                        ::interval])
+                       targets-exist?))
+
+(defn valid-config [data]
+  (when-not (s/valid? ::config config)
+    (throw (Exception. (s/explain-str ::config config))))
+  config)
